f36f055679193c1bf8b0163f9d6a96ebe1890e9a
migueloc22 <migueloc22@gmail.com>
2017-09-22 11:49:19 -0500

    modificaion de csUser

--- Munarca/Negocio/csUsuario.cs
+++ Munarca/Negocio/csUsuario.cs
@@ -23,6 +23,7 @@ namespace Negocio
         public int num_documento { get; set; }
         public int fk_id_tipo_doc { get; set; }
         public int fk_id_ciudad { get; set; }
+        public int codUser { get; set; }
         #endregion
         public csUsuario(int id_usuario, string nombre1, string nombre2, string apellido1, string apellido2, string correo, string foto, string direccion, string fecha_nacimiento, string telefono, int num_documento, int fk_id_tipo_doc, int fk_id_ciudad, string contraseña) 
          {
@@ -41,5 +42,23 @@ namespace Negocio
              this.telefono = telefono;
              this.contraseña = contraseña;
          }
+        public csUsuario(int id_usuario, string nombre1, string nombre2, string apellido1, string apellido2, string correo, string foto, string direccion, string fecha_nacimiento, string telefono, int num_documento, int fk_id_tipo_doc, int fk_id_ciudad, string contraseña ,int CodUer)
+        {
+            this.apellido1 = apellido1;
+            this.apellido2 = apellido2;
+            this.correo = correo;
+            this.direccion = direccion;
+            this.fecha_nacimiento = fecha_nacimiento;
+            this.fk_id_ciudad = fk_id_ciudad;
+            this.fk_id_tipo_doc = fk_id_tipo_doc;
+            this.foto = foto;
+            this.id_usuario = id_usuario;
+            this.nombre1 = nombre1;
+            this.nombre2 = nombre2;
+            this.num_documento = num_documento;
+            this.telefono = telefono;
+            this.contraseña = contraseña;
+            this.codUser = codUser;
+        }
     }
 }
