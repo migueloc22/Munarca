fa08d13739cf7ebb66d23f3b3e4d225eea510ea9
migueloc22 <migueloc22@gmail.com>
2017-10-10 19:36:14 -0500

    nuevods plugin

--- Munarca/Presentacion/js/JavaScript.js
+++ Munarca/Presentacion/js/JavaScript.js
@@ -1,3 +1,7 @@
 ﻿$(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
 });
+$('#ContentPlaceHolder1_ContentPlaceHolder1_txtFechaNac').datepicker({
+    formato: 'mm / dd / aaaa',
+    startDate: '-3d'
+});
