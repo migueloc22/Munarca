fe6fc80e29ec3e872e57279e6b1f33ef6dfdaa12
ViviRojasH <lvvrojas7@misena.edu.co>
2017-10-17 21:23:35 -0500

    logica categoria

--- Munarca/Negocio/LogicaCategoria.cs
+++ Munarca/Negocio/LogicaCategoria.cs
@@ -9,15 +9,17 @@ using BaseDatos;
 
 namespace Negocio
 {
-    public class LogicaCategoria :csRespuesta
+    public class LogicaCategoria : csRespuesta
     {
+        SqlConnection cnn;
         SqlCommand comman;
         SqlDataReader read;
         DataTable tabla;
-        public DataTable Listar() {
+        public DataTable Listar()
+        {
 
             tabla = new DataTable();
-            SqlConnection cnn=Conexion.AbrirCnn();
+            SqlConnection cnn = Conexion.AbrirCnn();
             try
             {
                 comman = new SqlCommand("select * from categoria", cnn);
@@ -32,7 +34,35 @@ namespace Negocio
                 return tabla;
             }
             finally { Conexion.CerrarCnn(cnn); }
+
+        }
+        // metodo para crear categoria
+        public bool crearCategoria(string categoria)
+        {
+            
+            cnn = Conexion.AbrirCnn();
+            bool retorno=false;
+            try
+            {
+                comman = new SqlCommand(@"insert into categoria(categoria) values(@categoria)", cnn);
+                comman.Parameters.AddWithValue("@categoria", categoria);
+                int ejecu= comman.ExecuteNonQuery();
+                if (ejecu > 0)
+                {
+                    retorno = true;
+                }
+                Conexion.CerrarCnn(cnn);
+                
+            }
+            catch (Exception ex)
+            {
+                rta = ex.ToString();
+            }
+
+            return retorno;
+                 
             
         }
+
     }
 }
