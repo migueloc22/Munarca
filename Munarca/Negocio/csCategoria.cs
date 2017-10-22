627a04d21acad3beaa55ab7494881fbfeed263ce
ViviRojasH <lvvrojas7@misena.edu.co>
2017-10-22 17:19:40 -0500

    agregaion de categoria

---
+++ Munarca/Negocio/csCategoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class csCategoria
+    {
+        public int idCategoria{ get; set; }
+        public string categoria { get; set; }
+       public csCategoria(int idCategoria, string categoria){
+           this.idCategoria = idCategoria;
+           this.categoria = categoria;
+
+}
+        
+    }
+
+}
