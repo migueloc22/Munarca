e619f4d4670f023670433fa7688934a1544cabc7
ViviRojasH <lvvrojas7@misena.edu.co>
2017-11-10 21:24:45 -0500

    Modificaciones en diseño

--- Munarca/Presentacion/UsusarioAdminn.master.designer.cs
+++ Munarca/Presentacion/UsusarioAdminn.master.designer.cs
@@ -58,6 +58,15 @@ namespace Presentacion {
         protected global::System.Web.UI.WebControls.HyperLink HyperLink5;
         
         /// <summary>
+        /// HyperLink6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink6;
+        
+        /// <summary>
         /// imgUer control.
         /// </summary>
         /// <remarks>
